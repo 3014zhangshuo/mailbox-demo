f416977004c043ccdc1cb5337f5589f0295c99ab
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-06 01:19:58 +0800

    add many of box select to index

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,10 +2,19 @@ class ConversationsController < ApplicationController
  before_action :authenticate_user!
  before_action :get_mailbox
  before_action :get_conversation, except: [:index]
+ before_action :get_box, only: [:inbox]
 
-  def index
-    @conversations = @mailbox.conversations.page(params[:page])
-  end
+ def index
+   if @box.eql? "inbox"
+     @conversations = @mailbox.inbox
+   elsif @box.eql? "sent"
+     @conversations = @mailbox.sentbox
+   else
+     @conversations = @mailbox.trash
+   end
+
+   @conversations = @conversations.page(params[:page])
+ end
 
   def show
   end
@@ -17,6 +26,14 @@ class ConversationsController < ApplicationController
   end
 
   private
+  
+  def get_box
+    if params[:box].blank? or !["inbox", "sent", "trash"].include?(params[:box])
+      params[:box] = 'inbox'
+    end
+
+    @box = params[:box]
+  end
 
     def get_mailbox
       @mailbox ||= current_user.mailbox
